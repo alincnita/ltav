b12c2ca2166fb1014fec59430ecc0fd2d75b1b5c
alincnita <alin.c.nita@gmail.com>
2023-07-04 02:04:57 +0300

    Initial commit
    Changes to be committed:
    	new file:   activitati.html
    	new file:   activitati/simpozion.pdf
    	new file:   admitere-cls-preg.html
    	new file:   admitere-cls-preg/acp1.pdf
    	new file:   admitere-cls-preg/acp2.pdf
    	new file:   admitere-cls-preg/acp3.pdf
    	new file:   admitere-cls-preg/acp4.docx
    	new file:   admitere-cls-preg/acp5.pdf
    	new file:   admitere-cls-preg/acp6.pdf
    	new file:   admitere-int-eng.html
    	new file:   admitere-int-eng/aie1.pdf
    	new file:   admitere-int-eng/aie2.pdf
    	new file:   admitere-int-eng/aie3.pdf
    	new file:   admitere-int-eng/aie4.pdf
    	new file:   admitere-int-eng/aie5.pdf
    	new file:   admitere-int-eng/aie6.pdf
    	new file:   admitere-liceu.html
    	new file:   admitere-liceu/al1.pdf
    	new file:   admitere-liceu/al2.pdf
    	new file:   anunturi/1.jpg
    	new file:   aripi.pdf
    	new file:   aripi.png
    	new file:   atestat_nita.doc
    	new file:   bacalaureat.html
    	new file:   bacalaureat/bac1.pdf
    	new file:   ceac.html
    	new file:   ceac/ceac1.pdf
    	new file:   ceac/ceac10.pdf
    	new file:   ceac/ceac11.pdf
    	new file:   ceac/ceac12.pdf
    	new file:   ceac/ceac13.pdf
    	new file:   ceac/ceac14.pdf
    	new file:   ceac/ceac15.pdf
    	new file:   ceac/ceac16.pdf
    	new file:   ceac/ceac17.pdf
    	new file:   ceac/ceac18.pdf
    	new file:   ceac/ceac19.pdf
    	new file:   ceac/ceac2.pdf
    	new file:   ceac/ceac20.pdf
    	new file:   ceac/ceac21.pdf
    	new file:   ceac/ceac22.pdf
    	new file:   ceac/ceac23.pdf
    	new file:   ceac/ceac24.pdf
    	new file:   ceac/ceac25.pdf
    	new file:   ceac/ceac3.pdf
    	new file:   ceac/ceac4.pdf
    	new file:   ceac/ceac5.pdf
    	new file:   ceac/ceac6.pdf
    	new file:   ceac/ceac7.pdf
    	new file:   ceac/ceac8.pdf
    	new file:   ceac/ceac9.pdf
    	new file:   consiliu.html
    	new file:   consiliu/consiliu1.pdf
    	new file:   consiliu/consiliu10.pdf
    	new file:   consiliu/consiliu11.pdf
    	new file:   consiliu/consiliu2.pdf
    	new file:   consiliu/consiliu3.pdf
    	new file:   consiliu/consiliu4.pdf
    	new file:   consiliu/consiliu5.pdf
    	new file:   consiliu/consiliu6.pdf
    	new file:   consiliu/consiliu7.pdf
    	new file:   consiliu/consiliu8.pdf
    	new file:   consiliu/consiliu9.pdf
    	new file:   contact.html
    	new file:   evaluare-nationala.html
    	new file:   evaluare-nationala/en1.pdf
    	new file:   evaluare-nationala/en2.pdf
    	new file:   evaluare-nationala/en3.pdf
    	new file:   galerie.html
    	new file:   galerie/g1.JPG
    	new file:   galerie/g2.JPG
    	new file:   galerie/g3.jpg
    	new file:   galerie/g4.jpg
    	new file:   index.html
    	new file:   inv-gimnazial.html
    	new file:   inv-liceal.html
    	new file:   inv-primar.html
    	new file:   istoric.html
    	new file:   motto.jpg
    	new file:   navbar.js
    	new file:   oferta.html
    	new file:   oferta.pdf
    	new file:   olimpiade-concursuri.html
    	new file:   organigrama.html
    	new file:   organigrama.pdf
    	new file:   personal-liceu.html
    	new file:   personal-liceu.pdf
    	new file:   plan-managerial.pdf
    	new file:   programe-culturale.html
    	new file:   programe-umanitare.html
    	new file:   rightbar.js
    	new file:   starea-inv.pdf
    	new file:   strategia-anticoruptie.doc
    	new file:   structura.pdf
    	new file:   stylesheet.css

---
+++ navbar.js
@@ -0,0 +1,43 @@
+document.write('\
+\
+    <a href="index.html"><div id="button">Acasă</div></a>\
+    <div class="dropdown" id="button">\
+        Prezentare\
+        <div class="dropdown_content">\
+            <a href="istoric.html">Scurt istoric</a><br><br>\
+            <a href="organigrama.html">Organigramă</a><br><br>\
+            <a href="oferta.html">Ofertă educaţională</a><br><br>\
+             <a href="personal-liceu.html">Personalul liceului</a>\
+        </div>\
+    </div>\
+    <div class="dropdown" id="button">Nivel de învăţământ\
+        <div class="dropdown_content">\
+            <a href="inv-primar.html">Învăţământ primar</a><br><br>\
+            <a href="inv-gimnazial.html">Învăţământ gimnazial</a><br><br>\
+            <a href="inv-liceal.html">Învăţământ liceal</a>\
+        </div>\
+    </div>\
+    <div class="dropdown" id="button">Admitere și înscriere\
+        <div class="dropdown_content">\
+            <a href="admitere-cls-preg.html">Clasa pregătitoare</a><br><br>\
+            <a href="admitere-liceu.html">Admitere liceu</a>\
+    </div>\
+    </div>\
+    <div class="dropdown" id="button">Examene naționale\
+        <div class="dropdown_content">\
+            <a href="evaluare-nationala.html">Evaluarea națională</a><br><br>\
+            <a href="bacalaureat.html">Bacalaureat</a>\
+        </div>\
+    </div>\
+    <div class="dropdown" id="button">Activităţi şi proiecte\
+        <div class="dropdown_content">\
+            <a href="activitati.html">Activităţi extracurriculare</a><br><br>\
+            <a href="olimpiade-concursuri.html">Olimpiade și concursuri</a><br><br>\
+            <a href="programe-culturale.html">Programe culturale</a><br><br>\
+            <a href="programe-umanitare.html">Programe umanitare</a>\
+        </div>\
+    </div>\
+    <a href="https://sites.google.com/site/asavlaicu/home"><div id="button">Asociaţia Sportivă</div></a>\
+    <a href="contact.html"><div id="button">Contact</div></a>\
+\
+');
---
+++ rightbar.js
@@ -0,0 +1,30 @@
+document.write('\
+\
+	<br>\
+	<div id="section_right">Documente utile</div>\
+    <ul>\
+        <li><a href="ceac.html">Documente CEAC</a></li>\
+		<li><a href="admitere-int-eng.html">Admitere clasa a V-a intensiv engleză</a></li>\
+        <li><a href="consiliu.html">Consiliu administraţie</a></li>\
+        <li><a href="plan-managerial.pdf">Plan managerial</a></li>\
+        <li><a href="strategia-anticoruptie.doc">Strategia anticorupţie</a></li>\
+        <li><a href="starea-inv.pdf">Starea învăţământului</a></li>\
+    </ul>\
+	<br>\
+\
+    <div id="section_right">Link-uri utile</div>\
+    <ul>\
+		<li><a href="structura.pdf">Structura anului şcolar 2017-2018</a></li>\
+		<li><a href="http://mateinfo.ro">MateInfo.ro</a></li>\
+    </ul>\
+	<br>\
+\
+    <a href="aripi.pdf"><div id="section_right">Revista liceului</div>\
+    <img src="aripi.png" width=210px height=100px/></a>\
+	<br><br><br>\
+\
+    <a href="galerie.html"><div id="section_right">Galerie foto-video</div></a>\
+    <img src="galerie/g1.jpg" width=210px height=120px/>\
+	<br><br>\
+\
+');
